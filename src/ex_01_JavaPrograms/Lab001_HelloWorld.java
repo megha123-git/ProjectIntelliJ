f9da2443767000dc8eec8c02fba202f58129da30
megha123-git <meghalinkedin24@gmail.com>
2026-06-05 19:31:01 +0530

    megha commit changes

--- src/ex_01_JavaPrograms/Lab001_HelloWorld.java
+++ src/ex_01_JavaPrograms/Lab001_HelloWorld.java
@@ -1,4 +1,11 @@
 package ex_01_JavaPrograms;
 
 public class Lab001_HelloWorld {
+    static void main(String[] args) {
+        System.out.println("Hello Megha01");
+        System.out.println("Hello Megha02");
+        System.out.println("Hello Megha03");
+        System.out.println("Hello Megha04");
+        System.out.println("Hello Hello Megha");
+    }
 }
